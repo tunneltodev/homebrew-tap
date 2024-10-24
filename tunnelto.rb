2f4cdc3f9dcf794eda487f337e6e13721a8a86a1
Alex Grinman <356333+agrinman@users.noreply.github.com>
2024-10-24 01:05:56 -0400

    Update tunnelto.rb (test)

--- tunnelto.rb
+++ tunnelto.rb
@@ -1,7 +1,7 @@
 class Tunnelto < Formula
     desc "Expose your local web server to the internet with a public url: https://tunnelto.dev"
     homepage "https://github.com/agrinman/tunnelto"
-    url "https://github.com/tunneltodev/tunnelto/archive/0.1.20.zip"
+    url "https://github.com/tunneltodev/tunnelto/archive/0.1.19.zip"
     sha256 "4730383204d6120da784d88d517a8464ea39f2ea66e75e00a2ce6147f964a038"
   
     depends_on "rust" => :build
